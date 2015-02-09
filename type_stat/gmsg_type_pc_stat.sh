dacef96a7574eddfb2f0c654f7c9cf2bbe06805b
k1ic <chenkai116@gmail.com>
2015-02-09 16:29:15 +0800

    群发类型统计脚本中调用}get_log_dates()生成日期列表
    群发类型统计脚本中调用}get_log_dates()生成日期列表

--- type_stat/gmsg_type_pc_stat.sh
+++ type_stat/gmsg_type_pc_stat.sh
@@ -2,103 +2,16 @@
 #从app log中统计粉服移动端群发私信类型占比情况
 #作者：陈凯
 
+#常量定义
 LOG_DIR="/data3/logs/prof/publicplatform";
 LOG_FILE_PRE="publicplatform";
+SCRIPT_DIR="/data1/fans_api_log/chenkai3/scripts/";
+
+#引入公用模块
+source ${SCRIPT_DIR}get_log_dates.sh
+
 
 #第一步：生成起始、结束时间段内的日期
-function get_log_dates() {
-    # 变量初始化
-    res_dates_str=""; #返回值
-    res_dates=(); #中间变量
-    
-    # 接收入参，起始、结束日期字符串，如：2014-11-23
-    s_date=$1;
-    e_date=$2;
- 
-    # 字符串分割为数组，分割符“-”
-    s_date_info=(${s_date//-/ });
-    e_date_info=(${e_date//-/ });
-
-    # 起始日期的年、月、日
-    s_year=${s_date_info[0]};
-    s_month=${s_date_info[1]};
-    s_day=${s_date_info[2]};
-
-    # 结束日期的年、月、日
-    e_year=${e_date_info[0]};
-    e_month=${e_date_info[1]};
-    e_day=${e_date_info[2]};
-
-    #echo ${s_date} ${e_date};
-    #echo ${s_year} ${s_month} ${s_day};
-    #echo ${e_year} ${e_month} ${e_day};
-
-    # 生成起止日期段内的所有日期
-    if [ ${s_month} -lt ${e_month} ] #waring： “[”和变量之间要有空格
-    then
-        #echo "<";
-        for((i=${s_month}; i<=${e_month}; ++i));
-        do
-            # 每月最后一天
-            last_day_of_month=`cal $i ${s_year} | grep -v "^$" | tail -n 1 | awk '{print $NF}'`; 
-            #echo ${last_day_of_month};
-
-            # 如果是起始月份
-            if [ $i -eq ${s_month} ]
-            then
-                for((j=${s_day}; j<=${last_day_of_month}; ++j));
-                do
-                    #格式化日期，月、日增加前导0
-                    date=`date -d "${s_year}-${s_month}-$j" "+%Y-%m-%d";`
-                    res_dates=( "${res_dates[@]}" ${date} ); #数组追加元素
-                done;
-
-            # 如果是中间月份
-            elif test $i -gt ${s_month} -a $i -lt ${e_month} 
-            then
-                for((j=1; j<=${last_day_of_month}; ++j));
-                do
-                    date=`date -d "${e_year}-$i-$j" "+%Y-%m-%d";`
-                    res_dates=( "${res_dates[@]}" ${date} );
-                done;
-
-            # 如果是结束月份
-            elif [ $i -eq ${e_month} ]
-            then
-                for((j=1; j<=${e_day}; ++j));
-                do
-                    date=`date -d "${e_year}-${e_month}-$j" "+%Y-%m-%d";`
-                    res_dates=( "${res_dates[@]}" ${date} );
-                done;
-            fi
-        done;
-    elif [ ${s_month} -eq ${e_month} ]
-    then
-        #echo "=";
-        for((j=${s_day}; j<=${e_day}; ++j));
-        do
-            date=`date -d "${s_year}-${s_month}-$j" "+%Y-%m-%d";`
-            res_dates=( "${res_dates[@]}" ${date} );
-        done;
-    elif [ ${s_month} -gt ${e_month} ]
-    then
-        echo "起始、结束日期写反啦";
-        exit;
-    fi
-
-    res_dates_str='';
-    # 遍历数组
-    for var in ${res_dates[@]};
-    do
-        #echo $var;
-        res_dates_str=${res_dates_str},$var;
-    done
-
-    #使用echo方式回传返回值
-    echo ${res_dates_str} | sed "s/^,//g";
-}
-
-#获取起始、结束时间段内的所有日期（逗号分隔的字符串）
 if [ ! -z $1 ]&&[ ! -z $2 ]
 then
     res_dates=`get_log_dates $1 $2;`
